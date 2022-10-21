ae68ea5105b4bfe1231cb8a1078a9185a8a4d216
KC Carrillo <100710273+spectrumcoder@users.noreply.github.com>
2022-10-21 10:06:59 -0500

    create newObjects.js

---
+++ practice/newObjects.js
@@ -0,0 +1,20 @@
+const desk = { 
+  color: "white", 
+  shape: "L shape", 
+  style: "modern", 
+  
+  drawer: { 
+    highlighers: 6, 
+    markers: 10, 
+    paperclips: 15, 
+    erasers: 2, 
+    "white-out": 2, 
+    staples: true, 
+    }, 
+  
+  organizer: { 
+    folders: 3, 
+    notebooks: 2, 
+    binders: 2, 
+  },
+}
